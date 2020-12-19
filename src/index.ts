f9eaf5586f5d93555c52bd18d2dfd01b008efd44
Jose Eduardo Gonçalves Silveira <joseeduardoampla@gmail.com>
2020-12-19 11:28:35 -0300

    add escape characters

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,14 @@ import axios from 'axios';
 import open from 'open';
 import cheerio from 'cheerio';
 import clipboardy from 'clipboardy';
+import chalk from 'chalk';
 
 const headers = {
   'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.101 Safari/537.36',
 };
 
 function getRequestUrl(query: string) {
-  const baseUrl = `https://www.google.com/search?sxsrf=ALeKk02hgZQm-GEdAwirFIHl6CSgPEOUZg%3A1608338316761&source=hp&ei=jEvdX_3aK_uz5OUPo_yU0AU&q=${query}&oq=${query}&gs_lcp=CgZwc3ktYWIQAzIFCAAQsQMyBQgAELEDMgUIABCxAzIFCAAQsQMyAggAMgIIADICCAAyAggAMgUIABCxAzICCAA6BwgjEOoCECc6BggjECcQEzoECCMQJzoICAAQsQMQgwE6BQguELEDOggIABDHARCvAToLCAAQsQMQxwEQowI6BAgAEEM6CAguELEDEIMBOggIABDHARCjAjoHCAAQsQMQQzoFCAAQywE6BggAEBYQHlCgCFiYGmCDPmgIcAB4AIAB1gGIAaMOkgEGMS4xMS4xmAEAoAEBqgEHZ3dzLXdperABCg&sclient=psy-ab&ved=0ahUKEwj90K-Q59jtAhX7GbkGHSM-BVoQ4dUDCAc&uact=5`;
+  const baseUrl = `https://www.google.com/search?sxsrf=ALeKk02hgZQm-GEdAwirFIHl6CSgPEOUZg%3A1608338316761&source=hp&ei=jEvdX_3aK_uz5OUPo_yU0AU&q=${encodeURI(query)}&oq=${encodeURI(query)}&gs_lcp=CgZwc3ktYWIQAzIFCAAQsQMyBQgAELEDMgUIABCxAzIFCAAQsQMyAggAMgIIADICCAAyAggAMgUIABCxAzICCAA6BwgjEOoCECc6BggjECcQEzoECCMQJzoICAAQsQMQgwE6BQguELEDOggIABDHARCvAToLCAAQsQMQxwEQowI6BAgAEEM6CAguELEDEIMBOggIABDHARCjAjoHCAAQsQMQQzoFCAAQywE6BggAEBYQHlCgCFiYGmCDPmgIcAB4AIAB1gGIAaMOkgEGMS4xMS4xmAEAoAEBqgEHZ3dzLXdperABCg&sclient=psy-ab&ved=0ahUKEwj90K-Q59jtAhX7GbkGHSM-BVoQ4dUDCAc&uact=5`;
   return baseUrl;
 }
 
@@ -27,15 +28,15 @@ function openAnswerInBrowser(query: string) {
     });
 
     if (uris.length !== 0) {
-      console.log('\n\nLinks encontrados:');
+      console.log(chalk.green('\n\nLinks encontrados:'));
 
       uris.forEach((uri) => {
-        console.log(uri);
+        console.log(chalk.green(uri));
       });
 
       open(uris[0]);
     } else {
-      console.log('Nenhuma resposta encontrada que contenha Brainly no link.');
+      console.log(chalk.red('Nenhuma resposta encontrada que contenha Brainly no link.\n'));
     }
   });
 }
@@ -43,16 +44,16 @@ function openAnswerInBrowser(query: string) {
 // Criamos uma array com as perguntas que já foram pesquisadas
 const linksPesquisados: string[] = [];
 
-console.log('Ouvindo por alterações no clipboard.');
+console.log(chalk.green('Ouvindo por alterações no clipboard.'));
 
 // Começamos o loop a cada 0.2s
 setInterval(() => {
   const text = clipboardy.readSync();
   if (text) {
-    console.log(`\n\nPergunta: ${text}`);
+    console.log(chalk.green(`\n\nPergunta: ${text}`));
 
     if (linksPesquisados.includes(text)) {
-      console.log('\nEssa pergunta já foi respondida, se deseja que ela seja respondida novamente, reinicie a aplicação.');
+      console.log(chalk.red('\nEssa pergunta já foi respondida, se deseja que ela seja respondida novamente, reinicie a aplicação.'));
     }
 
     clipboardy.writeSync('');
